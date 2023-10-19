c17be92cafbba668fb39f3eefea6aaf585386167
JBinin <namechenjiabin@icloud.com>
2023-10-20 00:08:35 +0800

    Add __init__.py

---
+++ csinference/__init__.py
@@ -0,0 +1,17 @@
+'''
+Author: JBinin namechenjiabin@icloud.com
+Date: 2023-10-08 18:02:40
+LastEditors: JBinin namechenjiabin@icloud.com
+LastEditTime: 2023-10-20 00:07:51
+FilePath: /CSInference/csinference/__init__.py
+Description: 
+
+Copyright (c) 2023 by icloud-ecnu, All Rights Reserved. 
+'''
+from csinference.algorithm.algorithm import NewFunctionCfg
+
+__version__ = "0.0.0"
+
+__all__ = [
+    "NewFunctionCfg"
+]
