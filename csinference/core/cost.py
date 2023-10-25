1b84b2a68a8303f99a4c98eb349545c6e57a55fc
JBinin <namechenjiabin@icloud.com>
2023-10-25 16:22:39 +0800

    Update cost

--- csinference/core/cost.py
+++ csinference/core/cost.py
@@ -2,7 +2,7 @@
 Author: JBinin namechenjiabin@icloud.com
 Date: 2023-10-09 22:27:45
 LastEditors: JBinin namechenjiabin@icloud.com
-LastEditTime: 2023-10-25 16:11:33
+LastEditTime: 2023-10-25 16:22:29
 FilePath: /CSInference/csinference/core/cost.py
 Description: 
 
@@ -16,26 +16,26 @@ from typing import List
 
 
 class FunctionCost():
-    def __init__(self, instance: Instance) -> None:
-        self.cpu = instance.cpu
-        self.mem = instance.mem
-        self.gpu = instance.gpu
+    def __init__(self) -> None:
         self.cpu_cost = 0.000127
         self.mem_cost = 0.0000127
         self.gpu_cost = 0.00011
         self.invocation_cost = 0.009 / 10000
 
-    def cost(self, duration: float, batch: int) -> float:
-        if self.gpu is None:
+    def cost(self, duration: float, batch: int, instance: Instance) -> float:
+        if instance.gpu is None:
             gpu = 0
         else:
-            gpu = self.gpu
+            gpu = instance.gpu
             duration = math.ceil(duration)
-        return (self.invocation_cost + (self.cpu * self.cpu_cost + self.mem * self.mem_cost + gpu * self.gpu_cost) * duration) / batch
+        return (self.invocation_cost +
+                (instance.cpu * self.cpu_cost +
+                 instance.mem * self.mem_cost +
+                    gpu * self.gpu_cost) * duration) / batch
 
-    def cost_with_distribution(self, time_out: float, rps: float, batch_max: int, lat_cal: Latency, instance) -> float:
+    def cost_with_distribution(self, time_out: float, rps: float, batch_max: int, lat_cal: Latency, instance: Instance) -> float:
         if batch_max == 1:
-            return self.cost(lat_cal.lat_avg(instance, 1), 1)
+            return self.cost(lat_cal.lat_avg(instance, 1), 1, instance)
         p = batch_distribution(rps, batch_max, time_out)
         return self.cost_with_probability(instance, p, lat_cal)
 
@@ -43,5 +43,5 @@ class FunctionCost():
         c = 0.0
         for i in range(len(probability)):
             c += self.cost(lat_cal.lat_avg(instance, i + 1),
-                           i+1) * probability[i]
+                           i+1, instance) * probability[i]
         return c
