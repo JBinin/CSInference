cf3353b66342b42346067d9f5f2d82d95e50004c
JBinin <namechenjiabin@icloud.com>
2023-11-06 01:47:05 +0800

    Add memory constraint

--- csinference/algorithm/algorithm.py
+++ csinference/algorithm/algorithm.py
@@ -1,8 +1,8 @@
 '''
 Author: JBinin namechenjiabin@icloud.com
 Date: 2023-10-08 17:10:16
-LastEditors: JBinin namechenjiabin@icloud.com
-LastEditTime: 2023-10-29 19:38:03
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2023-11-06 01:32:54
 FilePath: /CSInference/csinference/algorithm/algorithm.py
 Description: 
 
@@ -16,7 +16,7 @@ import os
 from abc import ABC, abstractmethod
 
 from csinference.core.cost import FunctionCost
-from csinference.core.util import Instance, Cfg, Cfgs, batch_distribution, get_timeout_list
+from csinference.core.util import Instance, Cfg, Cfgs, batch_distribution, get_timeout_list, Mem
 from csinference.core.latency import CPULatency, GPULatency
 
 
@@ -47,7 +47,7 @@ class CSInference(FunctionCfg):
         self.model_name = self.config['model_name']
         with open(os.path.join(self.config['cfg_path'], 'model.json'), 'r') as f:
             self.model_config = json.load(f)
-
+        self.mem_cal = Mem(self.model_config, self.model_name)
         self.get_lat_cal()
         self.get_cost_cal()
 
@@ -61,21 +61,25 @@ class CSInference(FunctionCfg):
     def get_cost_cal(self) -> None:
         self.cost_cal = FunctionCost()
 
-    def constraint_lat(self, time_out: float, instance: Instance, batch_size: int) -> bool:
+    def constraint_lat(self, time_out: float, instance: Instance, batch_size: int, slo : Union[float, None] = None) -> bool:
+        if slo is None:
+            slo = self.SLO
         if instance.gpu is None:
-            if time_out + self.cpu_lat_cal.lat_max(instance, batch_size) > self.SLO:
+            if time_out + self.cpu_lat_cal.lat_max(instance, batch_size) > slo:
                 return False
             return True
         else:
-            if time_out + self.gpu_lat_cal.lat_max(instance, batch_size) > self.SLO:
+            if time_out + self.gpu_lat_cal.lat_max(instance, batch_size) > slo:
                 return False
             return True
     
-    def get_max_timeout(self, instance : Instance, batch_size : int) -> float:
+    def get_max_timeout(self, instance : Instance, batch_size : int, slo : Union[float, None] = None) -> float:
+        if slo is None:
+            slo = self.SLO
         if instance.gpu is None:
-            return self.SLO - self.cpu_lat_cal.lat_max(instance, batch_size)
+            return slo - self.cpu_lat_cal.lat_max(instance, batch_size)
         else:   
-            return self.SLO - self.gpu_lat_cal.lat_max(instance, batch_size)
+            return slo - self.gpu_lat_cal.lat_max(instance, batch_size)
 
     def constraint_cost(self, cost):
         if cost > self.SLO:
@@ -86,8 +90,11 @@ class CSInference(FunctionCfg):
     def cpu_optimal_cost(self, Res_CPU: List, B_CPU: List[int], cpu_min: float, cpu_min_cfg: Union[Cfg, None], proportion : float) -> Tuple[float, Union[Cfg, None]]:
         R_CPU = self.arrival_rate * proportion
         for cpu in Res_CPU:
-            instance_cpu = Instance(cpu, 4 * cpu, None)
             for b in B_CPU:
+                mem = self.mem_cal.get_cpu_mem(cpu, b)
+                if mem is None:
+                    break
+                instance_cpu = Instance(cpu, mem, None)
                 for t in get_timeout_list(R_CPU, b, self.SLO):
                     
                     p = batch_distribution(R_CPU, b, t)
@@ -105,8 +112,14 @@ class CSInference(FunctionCfg):
     def gpu_optimal_cost(self, Res_GPU: List, B_GPU: List[int], gpu_min: float, gpu_min_cfg: Union[Cfg, None], proportion : float) -> Tuple[float, Union[Cfg, None]]:
         R_GPU = self.arrival_rate * proportion
         for gpu in Res_GPU:
-            instance_gpu = Instance(gpu / 3, gpu / 3 * 4, gpu)
             for b in B_GPU:
+                if self.mem_cal.get_gpu_gpu_mem(b) > gpu:
+                    break
+                cpu = gpu / 3
+                mem = self.mem_cal.get_gpu_mem(cpu, b)
+                if mem is None:
+                    break
+                instance_gpu = Instance(cpu, mem, gpu)
                 # TODO: support both A10 and T4
                 for t in get_timeout_list(R_GPU, b, self.SLO):
                     p = batch_distribution(R_GPU, b, t)
@@ -146,12 +159,24 @@ class CSInference(FunctionCfg):
         else:
             lat_cal = self.cpu_lat_cal
         cfgs = Cfgs()
+
+        if self.constraint == 'cost':
+            # if contraint is cost, find optimal Res from max to min
+            Res = list(reversed(Res))
+
         for res in Res:
-            if is_gpu:
-                ins = Instance(res / 3, res / 3 * 4, res)
-            else:
-                ins = Instance(res, 4 * res, None)
             for b in B:
+                if is_gpu:
+                    gpu = self.mem_cal.get_gpu_gpu_mem(b)
+                    if res < gpu:
+                        break
+                    cpu = res / 3
+                    mem = self.mem_cal.get_gpu_mem(cpu, b)
+                    if mem is None:
+                        break
+                    ins = Instance(cpu, mem, res)
+                else:
+                    ins = Instance(res, self.mem_cal.get_cpu_mem(res, b), None)
                 tau = (b-1)/rps
                 if self.constraint == 'lat': 
                     time_out = self.get_max_timeout(ins, b)
@@ -171,19 +196,29 @@ class CSInference(FunctionCfg):
                         time_out = 0
                     else:
                         #TODO: how to configure timeout for cost constraint
-                        time_out = tau
+                        time_out = min(tau, 5)
                     p = batch_distribution(rps, b, time_out)
                     lat = lat_cal.lat_with_probability(ins, p, time_out, tau)[1]
                     cost = self.cost_cal.cost_with_probability(ins, p, lat_cal)
-                    tmp = Cfgs(Cfg(ins, b, cost, rps, self.SLO, time_out, proportion, lat))
-                    if tmp.lat() <= cfgs.lat():
-                        cfgs = tmp
+                    if cost <= self.SLO:   
+                        if lat <= cfgs.lat():
+                            tmp = Cfg(ins, b, cost, rps, self.SLO, time_out, proportion, lat)
+                            tmp = self.optimize_cfg(tmp)
+                            cfgs = Cfgs(tmp)
+                        break
+            if self.constraint == 'cost':
+                c = cfgs.get_cfgs()
+                if len(c) != 0 and c[0] != None and c[0].batch_size == 1:
+                    break
+        
         new_cfg = None
         for cfg in cfgs.get_cfgs():
             if cfg is not None:
                 new_cfg = cfg
                 break
-        return self.optimize_cfg(new_cfg)
+        if self.constraint == 'lat':
+            new_cfg = self.optimize_cfg(new_cfg)
+        return new_cfg
     
 
     def optimize_cfg(self, cfg : Union[Cfg, None]) -> Union[Cfg, None]:
@@ -199,11 +234,18 @@ class CSInference(FunctionCfg):
             else:
                 lat = self.cpu_lat_cal.lat_with_probability(cfg.instance, p, t, (cfg.batch_size-1)/cfg.rps)[1]
                 cost = self.cost_cal.cost_with_probability(cfg.instance, p, self.cpu_lat_cal)
-            if cost <= cfg.cost:
-                cfg.cost = cost
-                cfg.latency = lat
-                cfg.timeout = t
+            if self.constraint == 'cost':
+                if cost <= self.SLO and lat < cfg.latency:
+                    cfg.cost = cost
+                    cfg.latency = lat
+                    cfg.timeout = t
+            elif self.constraint == 'lat':
+                if cost <= cfg.cost:
+                    cfg.cost = cost
+                    cfg.latency = lat
+                    cfg.timeout = t
         return cfg
+    
     
     def get_config_latency(self, Res_CPU: List, B_CPU: List[int], Res_GPU: List, B_GPU: List[int]):
         cfgs = Cfgs(self.get_config_with_one_platform(Res_CPU, B_CPU, False))
@@ -216,27 +258,6 @@ class CSInference(FunctionCfg):
             R_CPU = self.arrival_rate * alpha
             R_GPU = self.arrival_rate * beta
 
-            for gpu in Res_GPU:
-                instance_gpu = Instance(gpu / 3, gpu / 3 * 4, gpu)
-                for b_gpu in B_GPU:
-                    for t_gpu in get_timeout_list(R_GPU, b_gpu, cfgs.lat()):
-                        p_gpu = batch_distribution(R_GPU, b_gpu, t_gpu)
-                        gpu_lat = self.gpu_lat_cal.lat_with_probability(instance_gpu, p_gpu, t_gpu, (b_gpu-1)/R_GPU)[1]
-                        gpu_cost = self.cost_cal.cost_with_probability(instance_gpu, p_gpu, self.gpu_lat_cal)
-                        gpu_cfg =  Cfg(instance_gpu, b_gpu, gpu_cost, R_GPU, self.SLO, t_gpu, beta, gpu_lat)
-                        
-                        for cpu in Res_CPU:
-                            instance_cpu = Instance(cpu, 4 * cpu, None)
-                            for b_cpu in B_CPU:
-                                for t_cpu in get_timeout_list(R_CPU, b_cpu, cfgs.lat()):
-                                    p_cpu = batch_distribution(R_CPU, b_cpu, t_cpu)
-                                    cpu_lat = self.cpu_lat_cal.lat_with_probability(instance_cpu, p_cpu, t_cpu, (b_cpu-1)/R_CPU)[1]
-                                    cpu_cost = self.cost_cal.cost_with_probability(instance_cpu, p_cpu, self.cpu_lat_cal)
-                                    cpu_cfg = Cfg(instance_cpu, b_cpu, cpu_cost, R_CPU, self.SLO, t_cpu, alpha, cpu_lat)
-                                    
-                                    tmp = Cfgs(cpu_cfg, gpu_cfg)
-                                    if self.constraint_cost(tmp.cost()) is True and tmp.lat() < cfgs.lat():
-                                        cfgs = tmp
         return cfgs
 
     def get_config(self, arrival_rate: float, slo: Union[float, None] = None) -> Cfgs:
@@ -250,7 +271,7 @@ class CSInference(FunctionCfg):
         B_CPU_low, B_CPU_high = self.config["B_CPU"]
         B_CPU = list(range(B_CPU_low, B_CPU_high+1, 1))
         Res_CPU_low, Res_CPU_high = self.config["Res_CPU"]
-        Res_CPU = list(np.arange(Res_CPU_low, Res_CPU_high+1, 0.2))
+        Res_CPU = list(np.arange(Res_CPU_low, Res_CPU_high+1, 0.1))
 
         B_GPU_low, B_GPU_high = self.config["B_GPU"]
         B_GPU = list(range(B_GPU_low, B_GPU_high+1, 1))
--- csinference/core/util.py
+++ csinference/core/util.py
@@ -1,8 +1,8 @@
 '''
 Author: JBinin namechenjiabin@icloud.com
 Date: 2023-10-19 22:26:08
-LastEditors: JBinin namechenjiabin@icloud.com
-LastEditTime: 2023-10-29 16:41:03
+LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
+LastEditTime: 2023-11-06 01:37:23
 FilePath: /CSInference/csinference/core/util.py
 Description: 
 
@@ -48,6 +48,45 @@ def batch_distribution(lam: float, B: int, T: float) -> List[float]:
     return p
 
 
+class Mem:
+    def __init__(self, model_config: dict, model_name : str) -> None:
+        self.model_config = model_config[model_name]
+        # CPU
+        self.a, self.b = self.model_config["CPU"]["mem"]
+        # GPU
+        self.mem = self.model_config["GPU"]["mem"]
+        self.gpu_mem = self.model_config["GPU"]["gpu_mem"]
+
+    def get_mem(self, cpu, mem):
+        mem = mem / 1024
+        if cpu > mem * 4:
+            return None
+        elif cpu > mem:
+            mem = cpu
+        return mem
+
+    def get_cpu_mem(self, cpu : float, batch : int):
+        mem =  self.a * batch + self.b
+        mem = int(mem)
+        if mem % 64 != 0:
+            mem = ((mem // 64) + 1) * 64
+        return self.get_mem(cpu, mem)
+
+    def get_gpu_mem(self, cpu : float, batch : Union[int, None] = None):
+        mem = self.mem
+        mem = int(mem)
+        if mem % 64 != 0:
+            mem = ((mem // 64) + 1) * 64
+        return self.get_mem(cpu, mem)
+    
+    def get_gpu_gpu_mem(self, batch : int):
+        for i in range(len(self.gpu_mem)):
+            if batch <= self.gpu_mem[i]:
+                return i+1
+        return len(self.gpu_mem) + 1
+
+
+
 class Instance:
     def __init__(self, cpu: float, mem: Union[float, None], gpu: Union[int, None]) -> None:
         self.cpu = cpu
@@ -85,8 +124,8 @@ class Cfg:
             "rps:\t\t{%0.2f}" % self.rps + "\n" + \
             "timeout:\t{%0.2f}" % self.timeout + "\n" + \
             "proportion:\t{%0.2f}" % self.proportion + "\n" + \
-            "cost:\t\t{%0.2e}" % self.cost + "\n" \
-            "latency:\t{%0.2f}" % self.latency + "\n"
+            "cost:\t\t{%0.3e}" % self.cost + "\n" \
+            "latency:\t{%0.3f}" % self.latency + "\n"
         if self.instance.gpu is not None:
             ret = "gpu:\t\t{%d}" % self.instance.gpu + "\n" + ret
         return ret
@@ -95,6 +134,21 @@ class Cfg:
 class Cfgs:
     def __init__(self, *cfgs : Union[Cfg, None]) -> None:
         self.cfgs = cfgs
+
+    def get_resource(self):
+        for cfg in self.cfgs:
+            if cfg is not None:
+                if cfg.instance.gpu is not None:
+                    return cfg.instance.gpu, True
+                else:
+                    return cfg.instance.cpu, False
+        return None, False
+    
+    def get_batch(self):
+        for cfg in self.cfgs:
+            if cfg is not None:
+                return cfg.batch_size
+        return None
     
     def get_cfgs(self):
         return self.cfgs
@@ -127,7 +181,7 @@ class Cfgs:
                 slo = cfg.slo
         if ret == "":
             return "None\n"
-        ret = "total cost:\t{%0.2e}" % self.cost() + "\n" + \
-            "total latency:\t{%0.2f}" % self.lat() + "\n" + \
+        ret = "total cost:\t{%0.3e}" % self.cost() + "\n" + \
+            "total latency:\t{%0.3f}" % self.lat() + "\n" + \
             "slo:\t{%0.2e}" % slo+ "\n" + ret
         return "*" * 24 + "\n" + ret + "*" * 24 + "\n"
